b71f77297d2e1d06e27d255c30edecc4223e1edf
Kaci Childers <kaci.childers@gmail.com>
2017-01-30 23:46:53 +0000

    new code

--- script.js
+++ script.js
@@ -1,18 +1,91 @@
 TinyTurtle.apply(window);
 
 function square(){
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
+
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(10);
+forward(10);
+right(100);
+forward(115);
+right(180);
+forward(15);
+right(270);
+forward(10);
+right(10);
+forward(5);
+right(10);
+forward(5);
+right(10);
+forward(5);
+right(10);
+forward(5);
+right(10);
+forward(5);
+right(10);
+forward(5);
+right(10);
+forward(5);
+right(1);
+forward(5);
+right(1);
+forward(5);
+right(1);
+forward(5);
+right(5);
+forward(5);
+right(5);
+forward(5);
+right(5);
+forward(5);
+right(5);
+forward(5);
 }
+/*function triangle(){
+    right(45);
+    forward(50);
+    right(90);
+    forward(50); 
+    right(45); 
+    forward(50);
+    right(90); 
+    forward(20);
+    right(90);
+    
+; }*/
 
 // Type your function call below
+
+    square();
 
 
 stamp();
